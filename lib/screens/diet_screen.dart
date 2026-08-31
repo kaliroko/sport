d1d06ecc97c9fcf094c4bf0a7e07181be709dbab
torvalds <39930hvjj2@qq.com>
2026-08-31 05:58:36 +0000

    fix: 消除 SchoolType 重复定义导致的类型不兼容错误
    - 从 lib/utils/constants.dart 移除 SchoolType 枚举（保留在 user_profile.dart）
    - onboarding_screen: 移除 as constants 别名，SchoolType 统一从 user_profile.dart 解析
    - diet_screen + profile_screen: 添加 user_profile.dart 导入确保 SchoolType 来源唯一
    
    根本原因：SchoolType 在 constants.dart 和 user_profile.dart 中同时定义，
    导致 constants.SchoolType 与 user_profile.SchoolType 被视为不同类型。

--- lib/screens/diet_screen.dart
+++ lib/screens/diet_screen.dart
@@ -6,6 +6,7 @@ import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:metamorphosis_checkin/services/user_profile_service.dart';
 import 'package:metamorphosis_checkin/utils/constants.dart';
+import 'package:metamorphosis_checkin/models/user_profile.dart';
 import 'package:metamorphosis_checkin/theme/app_theme.dart';
 
 class DietScreen extends StatelessWidget {
