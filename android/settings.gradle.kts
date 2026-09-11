e8f3499fc765a9410be0f9ae13f6c2370696f137
kaliroko <2697177724@qq.com>
2026-09-11 16:01:57 +0000

    fix: 恢复 settings.gradle.kts pluginManagement 块，确保 CI 能找到 Flutter Gradle 插件

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,21 @@
+pluginManagement {
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdk = properties.getProperty("flutter.sdk")
+        check(flutterSdk != null) { "Flutter SDK not found. Define flutter.sdk in local.properties." }
+        flutterSdk
+    }
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.5.2" apply false
